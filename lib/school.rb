a78afb7015bf20dbf742699829ddcf1a62019e82
Princeton Rose <decoyzpr@gmail.com>
2019-03-13 18:30:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,15 @@
 # code here!
 class School
 
-  def initialize(name, roster = [])
+  def initialize(name, roster = {})
     @name = name
     @roster = roster
   end
-
+  
+ def add_student(student, grade)
+   if @roster[grade].include?(student) == false
+     @roster[grade] = student
+   end
+ end
 
 end
